81bbd7fc2a8ab95492852d82757195259eeb6f35
sensiz <dev.federicosensi@gmail.com>
2023-01-07 23:51:40 +0100

    initial Header + Hero

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <div class="container">
+        <nav class="d-flex justify-content-between align-items-center py-3">
+            <div class="logo d-flex align-items-center">
+                <img src="{{Vite::asset('resources/assets/images/profile.png')}}" alt="profile-photo" width="50px">
+                <h6 class="ps-3 m-0 text-uppercase">federico sensi</h6>
+            </div>
+            <div class="links d-flex">
+                <a href="#">
+                    <h6 class="text-uppercase m-0 pe-5">home</h6>
+                </a>
+                <a href="#">
+                    <h6 class="text-uppercase m-0 pe-5">about</h6>
+                </a>
+                <a href="#">
+                    <h6 class="text-uppercase m-0 pe-5">projects</h6>
+                </a>
+                <a href="#">
+                    <h6 class="text-uppercase m-0 pe-5">contact</h6>
+                </a>
+            </div>
+        </nav>
+    </div>
+</header>
