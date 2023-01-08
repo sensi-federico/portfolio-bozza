c6a8cea1c02fb8d5a065fa9ee58b132c688ebce3
sensiz <dev.federicosensi@gmail.com>
2023-01-08 01:47:39 +0100

    hero complete, start about

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 <!-- HERO SECTION -->
 <section class="hero d-flex align-items-center justify-content-center">
-    <div class="social d-flex flex-column bg-light p-1">
+    <div class="social d-flex flex-column bg-light">
         <!-- svg linkedin -->
         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
@@ -28,12 +28,12 @@
     </div>
     <div class="call-to-action d-flex flex-column align-items-center">
         <h1 class="text-center text-uppercase pt-2">Hey, I'm Federico Sensi</h1>
-        <p class="text-center pt-3">
+        <p class="text-center text-white pt-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem debitis quas temporibus beatae <br>
             sapiente aliquid excepturiarchitecto unde optio atque?
         </p>
         <div class="button text-center">
-            <p class="m-0 text-uppercase">projects</p>
+            <p class="m-0 text-uppercase text-black">projects</p>
         </div>
     </div>
 </section>
@@ -41,14 +41,39 @@
 
 
 <!-- ABOUT SECTION -->
-<section class="about pt-5">
+<section class="about py-5">
     <div class="text d-flex flex-column align-items-center">
         <h2 class="title text-uppercase text-center fw-bold">about me</h2>
         <div class="underline"></div>
-        <p class="text-center">
+        <p class="text-center mb-5 pb-5">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Obcaecati, assumenda deserunt ullam possimus quos similique expedita reiciendis illum veniam omnis.
         </p>
+    </div>
+    <div class="content container">
+        <div class="row">
+            <div class="col-5">
+                <h4 class="fw-bold">Get to know me!</h4>
+                <p>
+                    I'm a <span>Full-Stack Web Developer</span> building the Front-end of Websites and
+                    Web Applications that leads to the success of the overall product.
+                    Check out some of my work in the <span>Projects</span> section.
+                </p>
+                <p>
+                    I also like sharing content related to the stuff that I have learned over the years in
+                    <span>Web Development</span> so it can help other people of the Dev Community.
+                    Feel free to Connect or Follow me on my <a href="#">LinkedIn</a> where I post useful content related to Web Development and Programming
+                </p>
+                <p>
+                    I'm open to <span>Job</span> opportunities where I can contribute,learn and grow.
+                    If you have a good opportunity that matches my skills and experience then don't hesitate to <span>contact</span> me.
+                </p>
+                <div class="button">
+                    <p class="m-0 text-uppercase text-center text-black">contact</p>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
     </div>
 </section>
 <!-- /ABOUT SECTION -->
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header>
+<header class="bg-light">
     <div class="container">
         <nav class="d-flex justify-content-between align-items-center py-3">
             <div class="logo d-flex align-items-center">
